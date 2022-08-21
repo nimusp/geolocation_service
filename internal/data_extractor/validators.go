885141ce595c0780d650ac7a68f921b60fafeab9
Pavel Sumin <p.sumin7@gmail.com>
2022-08-21 16:44:49 +0300

    add column validators

--- internal/data_extractor/validators.go
+++ internal/data_extractor/validators.go
@@ -1,6 +1,10 @@
 package data_exctractor
 
 import (
+	"fmt"
+	"net"
+	"regexp"
+	"strconv"
 	"strings"
 )
 
@@ -13,4 +17,67 @@ func trimRow(row []string) (ip, countryCode, country, city, lat, lon, value stri
 	lon = strings.TrimSpace(row[longitudePosition])
 	value = strings.TrimSpace(row[mysteryValuePosition])
 	return
+}
+
+var (
+	countryCodeRE    = regexp.MustCompile(`^[A-Z]{2}$`) //  Alpha-2 code
+	countryAndCityRE = regexp.MustCompile(`^[A-Z]{1}[a-z ]+`)
+)
+
+const (
+	minLatitude  = -90.
+	maxLatitude  = 90.
+	minLongitude = -180.
+	maxLongitude = 180.
+)
+
+func isValidIP(ipAddress string) bool {
+	if len(ipAddress) == 0 {
+		return false
+	}
+
+	parsedAddr := net.ParseIP(ipAddress)
+	return parsedAddr != nil
+}
+
+func isValidCountryCode(countryCode string) bool {
+	return countryCodeRE.MatchString(countryCode)
+}
+
+func isValidCountry(country string) bool {
+	return countryAndCityRE.MatchString(country)
+}
+
+func isValidCity(city string) bool {
+	return countryAndCityRE.MatchString(city)
+}
+
+func parseLatitude(rawLat string) (float64, error) {
+	val, err := strconv.ParseFloat(rawLat, 64)
+	if err != nil {
+		return 0, err
+	}
+
+	if val < minLatitude || val > maxLatitude {
+		return 0, fmt.Errorf("invalid latitude value %v", val)
+	}
+
+	return val, nil
+}
+
+func parseLongitude(rawLon string) (float64, error) {
+	val, err := strconv.ParseFloat(rawLon, 64)
+	if err != nil {
+		return 0, err
+	}
+
+	if val < minLongitude || val > maxLongitude {
+		return 0, fmt.Errorf("invalid longitude value %v", val)
+	}
+
+	return val, nil
+}
+
+func parseMysteryValue(rawValue string) (int64, error) {
+	return strconv.ParseInt(rawValue, 10, 64)
 }
--- internal/data_extractor/validators_test.go
+++ internal/data_extractor/validators_test.go
@@ -63,3 +63,291 @@ func Test_trimRow(t *testing.T) {
 		})
 	}
 }
+
+func Test_isValidIP(t *testing.T) {
+	testCases := []struct {
+		name string
+		ip   string
+		want bool
+	}{
+		{
+			name: "empty IP",
+			ip:   "",
+			want: false,
+		},
+		{
+			name: "bad IP",
+			ip:   "bad IP",
+			want: false,
+		},
+		{
+			name: "IP v4",
+			ip:   "127.0.0.1",
+			want: true,
+		},
+		{
+			name: "IP v6",
+			ip:   "2001:0db8:85a3:0000:0000:8a2e:0370:7334",
+			want: true,
+		},
+	}
+
+	for _, tc := range testCases {
+		t.Run(tc.name, func(t *testing.T) {
+			got := isValidIP(tc.ip)
+
+			assert.Equal(t, tc.want, got)
+		})
+	}
+}
+
+func Test_isValidCountryCode(t *testing.T) {
+	testCases := []struct {
+		name string
+		code string
+		want bool
+	}{
+		{
+			name: "empty code",
+			code: "",
+			want: false,
+		},
+		{
+			name: "invalid code 1",
+			code: "A",
+			want: false,
+		},
+		{
+			name: "invalid code 2",
+			code: "aa",
+			want: false,
+		},
+		{
+			name: "Alpha 3 format",
+			code: "NLD",
+			want: false,
+		},
+		{
+			name: "falid",
+			code: "NL",
+			want: true,
+		},
+	}
+
+	for _, tc := range testCases {
+		t.Run(tc.name, func(t *testing.T) {
+			got := isValidCountryCode(tc.code)
+
+			assert.Equal(t, tc.want, got)
+		})
+	}
+}
+
+func Test_isValidCountry(t *testing.T) {
+	testCases := []struct {
+		name    string
+		country string
+		want    bool
+	}{
+		{
+			name:    "empty",
+			country: "",
+			want:    false,
+		},
+		{
+			name:    "first latter is lowercased",
+			country: "netherlands",
+			want:    false,
+		},
+		{
+			name:    "with space",
+			country: "United Kingdom of Great Britain and Northern Ireland",
+			want:    true,
+		},
+		{
+			name:    "valid",
+			country: "Netherlands",
+			want:    true,
+		},
+	}
+
+	for _, tc := range testCases {
+		t.Run(tc.name, func(t *testing.T) {
+			got := isValidCountry(tc.country)
+
+			assert.Equal(t, tc.want, got)
+		})
+	}
+}
+
+func Test_isValidCity(t *testing.T) {
+	testCases := []struct {
+		name string
+		city string
+		want bool
+	}{
+		{
+			name: "empty",
+			city: "",
+			want: false,
+		},
+		{
+			name: "first latter is lowercased",
+			city: "amsterdam",
+			want: false,
+		},
+		{
+			name: "with space",
+			city: "Kuala Lumpur",
+			want: true,
+		},
+		{
+			name: "valid",
+			city: "Amsterdam",
+			want: true,
+		},
+	}
+
+	for _, tc := range testCases {
+		t.Run(tc.name, func(t *testing.T) {
+			got := isValidCity(tc.city)
+
+			assert.Equal(t, tc.want, got)
+		})
+	}
+}
+
+func Test_parseLatitude(t *testing.T) {
+	testCases := []struct {
+		name     string
+		latitude string
+		want     float64
+		wantErr  bool
+	}{
+		{
+			name:     "empty",
+			latitude: "",
+			want:     0,
+			wantErr:  true,
+		},
+		{
+			name:     "not float",
+			latitude: "hello",
+			want:     0,
+			wantErr:  true,
+		},
+		{
+			name:     "invalid value 1",
+			latitude: "-91",
+			want:     0,
+			wantErr:  true,
+		},
+		{
+			name:     "invalid value 2",
+			latitude: "91",
+			want:     0,
+			wantErr:  true,
+		},
+		{
+			name:     "valid",
+			latitude: "-1.234",
+			want:     -1.234,
+			wantErr:  false,
+		},
+	}
+
+	for _, tc := range testCases {
+		t.Run(tc.name, func(t *testing.T) {
+			got, gotErr := parseLatitude(tc.latitude)
+
+			assert.Equal(t, tc.wantErr, gotErr != nil)
+			assert.Equal(t, tc.want, got)
+		})
+	}
+}
+
+func Test_parseLongitude(t *testing.T) {
+	testCases := []struct {
+		name      string
+		longitude string
+		want      float64
+		wantErr   bool
+	}{
+		{
+			name:      "empty",
+			longitude: "",
+			want:      0,
+			wantErr:   true,
+		},
+		{
+			name:      "not float",
+			longitude: "hello",
+			want:      0,
+			wantErr:   true,
+		},
+		{
+			name:      "invalid value 1",
+			longitude: "-182",
+			want:      0,
+			wantErr:   true,
+		},
+		{
+			name:      "invalid value 2",
+			longitude: "182",
+			want:      0,
+			wantErr:   true,
+		},
+		{
+			name:      "valid",
+			longitude: "11",
+			want:      11,
+			wantErr:   false,
+		},
+	}
+
+	for _, tc := range testCases {
+		t.Run(tc.name, func(t *testing.T) {
+			got, gotErr := parseLongitude(tc.longitude)
+
+			assert.Equal(t, tc.wantErr, gotErr != nil)
+			assert.Equal(t, tc.want, got)
+		})
+	}
+}
+
+func Test_parseMysteryValue(t *testing.T) {
+	testCases := []struct {
+		name    string
+		value   string
+		want    int64
+		wantErr bool
+	}{
+		{
+			name:    "empty",
+			value:   "",
+			want:    0,
+			wantErr: true,
+		},
+		{
+			name:    "not int",
+			value:   "hello",
+			want:    0,
+			wantErr: true,
+		},
+		{
+			name:    "valid",
+			value:   "111",
+			want:    111,
+			wantErr: false,
+		},
+	}
+
+	for _, tc := range testCases {
+		t.Run(tc.name, func(t *testing.T) {
+			got, gotErr := parseMysteryValue(tc.value)
+
+			assert.Equal(t, tc.wantErr, gotErr != nil)
+			assert.Equal(t, tc.want, got)
+		})
+	}
+}
